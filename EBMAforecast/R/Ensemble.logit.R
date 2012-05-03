bd67e9aa763431a442250a78e5010fb0bb7d021c
Jacob Montgomery <jacob.montg@gmail.com>
2012-05-03 12:15:49 -0500

    Forgot to close files before push

--- EBMAforecast/R/Ensemble.logit.R
+++ EBMAforecast/R/Ensemble.logit.R
@@ -34,7 +34,26 @@ getClass()
 showMethods()
 existsMethod()
 
+                                        #' The length of a string (in characters).
+                                        #'
+                                        #' @param string input character vector
+                                        #' @return numeric vector giving number of characters in each element of the
+                                        #'   character vector.  Missing string have missing length.
+                                        #' @keywords character
+                                        #' @seealso \code{\link{nchar}} which this function wraps
+                                        #' @export 
+                                        #' @examples
+                                        #' str_length(letters)
+                                        #' str_length(c("i", "like", "programming", NA))
+          str_length <- function(string) { 
+            string <- check_string(string) 
+            nc <- nchar(string, allowNA = TRUE)
+            is.na(nc) <- is.na(string)
+            nc
+          } 
 
+
+          
 Ensemble.logit <-
 function(y, pp.raw, exp=3, tol=.001, max.iter=10000, model.names=NULL){
 
