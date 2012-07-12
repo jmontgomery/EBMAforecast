45c0cd643ea9afe6ebd665cdbbd4f88399fdc400
Florian Hollenbach <florian.hollenbach@gmail.com>
2012-07-12 10:52:27 -0400

    Erikson Wlezien added to Rdata

--- PresForecastApsa/creating_predictions.R
+++ PresForecastApsa/creating_predictions.R
@@ -108,3 +108,14 @@ insample.data<-merge(insample.data,berry,by="Year",all.x=TRUE,all.y=TRUE)
 
 save(insample.data, file="insample.data.RData")
 
+rm(list=ls(all=TRUE))
+setwd("~/Documents/GIT/EBMAforecast/PresForecastApsa")
+
+load("insample.data.RData")
+erikson.insample<-read.csv("Erikson_Wlezien_forecast.csv")
+
+head(insample.data)
+insample.data<-merge(insample.data,erikson.insample,by="Year",all.x=TRUE,all.y=TRUE)
+
+insample.data
+save(insample.data,file="insample.data.RData")
