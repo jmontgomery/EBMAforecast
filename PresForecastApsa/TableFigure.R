aec7952fbeac01dbb87afcf9bfd98126a6baa8c2
Jacob Montgomery <jacob.montg@gmail.com>
2012-07-12 08:51:32 -0500

    Table and Figure for PS

--- PresForecastApsa/TableFigure.R
+++ PresForecastApsa/TableFigure.R
@@ -7,33 +7,34 @@ setwd("~/GITHUB/EBMAforecast/")
 current.code <- as.package("EBMAforecast")
 load_all(current.code)
 document(current.code)
-ensemble <- calibrateEnsemble(.forecastData=thisFD, model="normal", tol=1.490116e-08, maxIter=1000000, useModelParams=TRUE, predType="posteriorMedian")
 
 
-plot(ensemble, subset=15, main=c("2008 Election"), xLab="% Two Party Vote for Bush", col=rep("gray40", 8))                        
-par(mfrow=c(2,1))
-plot(ensemble, subset=14:15, main=c("2004 Election", "2008 Election"), xLab="% Two Party Vote for Bush", col=rep("gray40", 8))                        
 
 setwd("~/GITHUB/EBMAforecast/PresForecastApsa/")
-
-insample <- read.csv("insample.csv", row.names=2)
-insample <- insample[,-1]
+load("/Users/jmontgomery/Github/EBMAforecast/PresForecastApsa/insample.data.RData")
+insample.data <- insample.data[-34,]
+rownames(insample.data) <- insample.data$Year
 my.years <- paste(seq(1952,2008, by=4))
-this.pred <- insample[my.years,]
-
+this.pred <- insample.data[my.years, -c(1, 7, 8)]
+this.pred
 
 setwd("~/Dropbox/EBMA/ReplicationFiles/ForPADataverse/")
 master.data <- read.csv("presdata.csv", row.names=1)
 this.out <- (master.data[my.years, "dv"])
 
-trial <- matrix(c(1,2,3,4,5,6,7,8), nrow=2, ncol=8)
-trial2 <- c(0, 2)
-thisFD <- makeForecastData(.predCalibration=this.pred, .outcomeCalibration=this.out, .predTest=trial, .outcomeTest=trial2, .modelNames=colnames(this.pred))
-ensemble <- calibrateEnsemble(.forecastData=thisFD, model="normal", tol=1.490116e-08, maxIter=1000000, useModelParams=TRUE, predType="posteriorMedian")
-summary(ensemble, showCoefs=FALSE)
-summary(ensemble, showCoefs=FALSE, period="test")
 
-par(mar=c(2,2,2,1), tcl=0, mgp=c(1,0,0))
+
+thisFD <- makeForecastData(.predCalibration=this.pred, .outcomeCalibration=this.out, .modelNames=colnames(this.pred))
+ensemble <- calibrateEnsemble(.forecastData=thisFD, model="normal", tol=1.490116e-08, maxIter=1000000, useModelParams=FALSE, predType="posteriorMedian")
+summary(ensemble, showCoefs=FALSE)
+
+setwd("~/GITHUB/EBMAforecast/PresForecastApsa/")
+pdf(file="Figure1.pdf", width=5, height=6)
+par(mar=c(2,2,2,1), tcl=0, mgp=c(1,0,0), mfrow=c(2,1))
+plot(ensemble, subset=14:15, main=c("2004 Election", "2008 Election"), xLab="% Two Party Vote for Incumbent", col=rep("gray40", 8))                        
+dev.off()
+
+
 
                         
 
