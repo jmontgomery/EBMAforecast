88d7f14a1910d33658c96336173f02f611f07b2e
Florian Hollenbach <florian.hollenbach@gmail.com>
2012-07-27 18:08:33 -0400

    coi data added

--- APSA_2012/Data/Economic_predictions.R
+++ APSA_2012/Data/Economic_predictions.R
@@ -63,3 +63,75 @@ head(true_un)
 ##now merge true data and forecasts
 unemployment_data<-merge(true_un, unemployment_forecast, by=("year.quarter"),all.x=TRUE,all.y=TRUE)
 save(unemployment_data,file="unemployment_data.RData")
+
+
+
+############ do the same for cpi data
+rm(list=ls(all=TRUE))
+setwd("~/Documents/GIT/EBMAforecast/APSA_2012/Data")
+library(reshape)
+
+
+cpi<-read.csv("CPI.csv")
+#keep only 1 and 4 quarter predictions
+cpi<-cpi[,c("year","quarter","id","industry","CPI3","CPI6")]
+head(cpi)
+summary(cpi)
+## replace -999 with NA
+cpi$id<-ifelse(cpi$id==-999,NA,cpi$id)
+cpi$industry<-ifelse(cpi$industry==-999,NA,cpi$industry)
+cpi$CPI3<-ifelse(cpi$CPI3==-999,NA,cpi$CPI3)
+cpi$CPI6<-ifelse(cpi$CPI6==-999,NA,cpi$CPI6)
+
+
+#create year.quarter variable
+cpi$year.quarter<-paste(cpi$year,cpi$quarter,sep=".")
+head(cpi)
+cpi<-subset(cpi,year>1982)
+
+##drop year, quarter and industry for now
+cpi<-cpi[,-c(1,2,4)]
+
+melt=melt(cpi,id.vars=c("id","year.quarter"))
+head(melt)
+
+cpi_forecast=cast(melt,year.quarter +variable ~ id)
+head(cpi_forecast)
+summary(cpi_forecast)
+cpi_forecast$year.quarter<-as.numeric(cpi_forecast$year.quarter)
+summary(cpi_forecast)
+
+cpi_true<-read.csv("True_cpi.csv")
+head(cpi_true)
+## drop all vintages before 2012 may
+cpi_true<-cpi_true[,c("X","Realiz5")]
+
+head(cpi_true)
+
+# mark NAs, divide by 100 to bring to % points
+#cpi_true$Realiz5<-ifelse(cpi_true$Realiz5=="#N/A",NA,cpi_true$Realiz5)
+new<-t(as.data.frame((strsplit(as.character(cpi_true$X), ":"))))
+new<-as.data.frame(new)
+names(new)<-c("year","quarter")
+
+dim(new)
+
+cpi_true<-data.frame(cbind(cpi_true,new))
+head(cpi_true)
+cpi_true$year<-(as.numeric(cpi_true$year)+1967)
+cpi_true$quarter<-as.numeric(cpi_true$quarter)
+head(cpi_true)
+true_cpi<-cpi_true[,c("Realiz5","year","quarter")]
+true_cpi<-subset(true_cpi,true_cpi$year>1982)
+summary(true_cpi)
+true_cpi$year.quarter<-paste(true_cpi$year,true_cpi$quarter,sep=".")
+true_cpi<-true_cpi[,-c(2,3)]
+head(true_cpi)
+true_cpi$year.quarter<-as.numeric(true_cpi$year.quarter)
+
+##now merge true data and forecasts
+cpi_data<-merge(true_cpi, cpi_forecast, by=("year.quarter"),all.x=TRUE,all.y=TRUE)
+save(cpi_data,file="cpi_data.RData")
+
+
+
