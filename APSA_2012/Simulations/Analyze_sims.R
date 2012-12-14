f0b3dfe079ca8cdf785789d537a1b7697fa09003
Jacob Montgomery <jacob.montg@gmail.com>
2012-12-14 16:09:51 -0600

    New analysis code

--- APSA_2012/Simulations/Analyze_sims.R
+++ APSA_2012/Simulations/Analyze_sims.R
@@ -3,7 +3,7 @@ rm(list=ls(all=TRUE))
 #setwd("~/Documents/GIT/EBMAforecast/APSA_2012/Simulations")
 setwd("~/Github/EBMAforecast/APSA_2012/Simulations")
 
-load("~/Dropbox/EBMA/ResultsOfSims")
+load("SecondRound")
 objects()
 
 summary(output)
@@ -12,22 +12,35 @@ summary(output)
 ##### here is the function that plots seven points with a lowes line for each statistic
 plot.fn<-function(models, train, func){
 
-c<-c(0,0.01,0.025,0.05,0.1,0.2,0.5)
-slots<-rep(NA,7)
+
+ alpha<-c(10,5,3)
+  if (models>3){alpha<-c(alpha, rep(1, models-3))}
+ a.mean <- alpha/mean(alpha)
+  
+c<-c(0,.01, 0.02,.025, 0.03, 0.04, .05, 0.075, 0.1, 0.15, 0.2, 0.3, .5)
+slots<-rep(NA,13)
 count<-0
-for(i in 1:833){
+for(i in 1:1911){
 	if(output[[i]]$theseParams[1]==train & output[[i]]$theseParams[2]==models){
 	count<-count+1
 	slots[count]<-i} 
 	}
-error.mat<-mae.mat<-rmse.mat<-matrix(NA,nrow=7,ncol=1000)
+error.mat<-mae.mat<-rmse.mat<-mad.mat<-rmsle.mat <- mape.mat <- meape.mat <- mrae.mat <- pw.mat <- crps.mat <- matrix(NA,nrow=13,ncol=100)
 
 for(i in slots){
 	count<-which(slots==i)
-	error.all.mat<-rowSums(abs(output[[i]]$error))
+        errror.all.mat<-abs(output[[i]]$error)%*%a.mean
+	#error.all.mat<-rowSums(abs(output[[i]]$error))
 	error.mat[count,]<-output[[i]]$error[,1]
 	rmse.mat[count,]<-output[[i]]$pred.rmse[,"EBMA"]
 	mae.mat[count,]<-output[[i]]$pred.mae[,"EBMA"]
+        mad.mat[count,]<-output[[i]]$pred.mad[,"EBMA"]
+        rmsle.mat[count,] <- output[[i]]$pred.rmsle[,"EBMA"]
+        mape.mat[count,] <- output[[i]]$pred.mape[,"EBMA"]
+        meape.mat[count,] <- output[[i]]$pred.meape[,"EBMA"]
+        mrae.mat[count,] <- output[[i]]$pred.mrae[,"EBMA"]
+        pw.mat[count,] <- output[[i]]$pred.pw[,"EBMA"]
+        crps.mat[count,] <- output[[i]]$model.crps
 }
 
 if(func=="med"){
@@ -35,36 +48,66 @@ if(func=="med"){
 	error<-(apply(error.mat,1,FUN=median, na.rm=TRUE))
 	rmse<-abs(apply(rmse.mat,1,FUN=median, na.rm=TRUE))
 	mae<-abs(apply(mae.mat,1,FUN=median, na.rm=TRUE))
-}
+      	mad<-abs(apply(mad.mat,1,FUN=median, na.rm=TRUE))
+        rmsle<-abs(apply(rmsle.mat,1,FUN=median, na.rm=TRUE))
+        mape<-abs(apply(mape.mat,1,FUN=median, na.rm=TRUE))
+        meape<-abs(apply(meape.mat,1,FUN=median, na.rm=TRUE))
+        mrae<-abs(apply(mrae.mat,1,FUN=median, na.rm=TRUE))
+        pw<-abs(apply(pw.mat,1,FUN=median, na.rm=TRUE))
+        crps<-abs(apply(crps.mat,1,FUN=median, na.rm=TRUE))
+      }
 
 if(func=="mea"){
-	error.all<-apply(abs(error.mat),1,FUN=mean)
-	error<-apply((error.mat),1,FUN=mean)
-	rmse<-apply(rmse.mat,1,FUN=mean)
-	mae<-apply(mae.mat,1,FUN=mean)
-}
-par(mfrow=c(2,2))
-plot(c,error.all,pch=15,main="Mean Abs Weight Error (all models)")
+	error.all<-apply(abs(error.mat),1,FUN=mean,na.rm=TRUE)
+	error<-(apply(error.mat,1,FUN=mean, na.rm=TRUE))
+	rmse<-abs(apply(rmse.mat,1,FUN=mean, na.rm=TRUE))
+	mae<-abs(apply(mae.mat,1,FUN=mean, na.rm=TRUE))
+      	mad<-abs(apply(mad.mat,1,FUN=mean, na.rm=TRUE))
+        rmsle<-abs(apply(rmsle.mat,1,FUN=mean, na.rm=TRUE))
+        mape<-abs(apply(mape.mat,1,FUN=mean, na.rm=TRUE))
+        meape<-abs(apply(meape.mat,1,FUN=mean, na.rm=TRUE))
+        mrae<-abs(apply(mrae.mat,1,FUN=mean, na.rm=TRUE))
+        pw<-abs(apply(pw.mat,1,FUN=mean, na.rm=TRUE))
+        crps<-abs(apply(crps.mat,1,FUN=mean, na.rm=TRUE))
+      }
+
+
+
+par(mfrow=c(3,3))
+plot(c,error.all,pch=15,main="Weighted Weight Error (all)")
 lines(loess(error.all~c))
 plot(c,error,pch=15,main="Weight Error (largest weight model)")
 abline(h=0)
 lines(loess(error~c))
+plot(c,crps,pch=15,main="CRPS")
+lines(loess(crps~c))
 plot(c,mae,pch=15,main="MAE")
 lines(loess(mae~c))
 plot(c,rmse,pch=15,main="RMSE")
 lines(loess(rmse~c))
-mae
+plot(c,mape,pch=15,main="MAPE")
+lines(loess(mape~c))
+plot(c,meape,pch=15,main="MEAPE")
+lines(loess(meape~c))
+plot(c,mrae,pch=15,main="MRAE")
+lines(loess(mrae~c))
+plot(c,pw,pch=15,main="PW")
+lines(loess(pw~c))
+
+
+
 }
 
 
 
-nTrain<-c(3:15,20,50,57,100)
+nTrain<-c(3:15,20,25,35,45,55,65,85,100)
 nmod<-seq(3,15, by=2)
-constant<-c(0,.01, .025, .05, .1, .2, .5)
+constant<-c(0,.01, 0.02,.025, 0.03, 0.04, .05, 0.075, 0.1, 0.15, 0.2, 0.3, .5)
 iter<-100
-outSample<-100
+outSample<-250
+
 #to plot, specify the number of models and number of training observation in the sim data that one wants to plot and where the mean (command "mea") or median (command "med") of the 1000 iteration for each statistic, abs error all models is the difference in weights to true weights for all models, abs error largest weight model is only the difference in weight to true weight for the largest model
-j <- plot.fn(models=5,train=5,func="mea")
+j <- plot.fn(models=11,train=5,func="med")
 
 #this creates a three dataframes with one row per parameter combination, the columns are number of training set obs, number of models, mean statistic (over iterations), median statistic (over iterations), variance of statistic (over iterations) and value of the constant
 
