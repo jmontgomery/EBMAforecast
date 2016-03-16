9d5fd9b4373229f621833c69b124fd09f6f070f1
Florian M. Hollenbach <florian.hollenbach@gmail.com>
2016-03-16 14:20:11 -0500

    rgibbs

--- TryGibbs.R
+++ TryGibbs.R
@@ -7,16 +7,31 @@ outcomeCalibration=calibrationSample[,"Insurgency"]
 W = c(1/3,1/3,1/3)
 
 x2 = GibbsLogit(outcomeCalibration,predCalibration,W,500)
-theta = matrix(NA, nrow = dim(predCalibration)[1], ncol = dim(predCalibration)[2])
-for(i in 1: dim(predCalibration)[1]){
-  if(outcomeCalibration[i] == 1){theta[i,] = W*predCalibration[i,]/sum(W*predCalibration[i,])}
-  if(outcomeCalibration[i] == 0){theta[i,] = W*(1 -predCalibration[i,])/sum(W*(1- predCalibration[i,]))}
+
+iterations = 10000
+w_post = matrix(NA, nrow = iterations, ncol = dim(predCalibration)[2])
+W = c(1/3,1/3,1/3)
+for(iter in 1:iterations){
+  theta = matrix(NA, nrow = dim(predCalibration)[1], ncol = dim(predCalibration)[2])
+  for(i in 1: dim(predCalibration)[1]){
+    if(outcomeCalibration[i] == 1){theta[i,] = W*predCalibration[i,]/sum(W*predCalibration[i,])}
+    if(outcomeCalibration[i] == 0){theta[i,] = W*(1 -predCalibration[i,])/sum(W*(1- predCalibration[i,]))}
+  }
+  
+  t = matrix(NA, nrow = dim(predCalibration)[1], ncol = dim(predCalibration)[2])
+  for(i in 1: dim(predCalibration)[1]){
+    t[i,] = rmultinom(1, 1, theta[i,])
+  }
+  nu = rep(NA,dim(predCalibration)[2])
+  for(m in 1: dim(t)[2]){
+    nu[m] = 1 + sum(t[,m])
+  }  
+  W = MCMCpack::rdirichlet(1, nu)
+  w_post[iter,] = W
 }
   
-t = matrix(NA, nrow = dim(predCalibration)[1], ncol = dim(predCalibration)[2])
-for(i in 1: dim(predCalibration)[1]){
-  t[i,] = rmultinom(1, 1, theta[i,])
-}
+  
+  
   x2[["theta_post"]][[1]][696,]
 theta[696,]
 x2[["W_post"]]
