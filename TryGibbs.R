d5760870fd6542b4df624178c53d541c2d28172a
fhollenbach <florian.hollenbach@gmail.com>
2016-03-25 09:00:59 -0500

    working sampler

--- TryGibbs.R
+++ TryGibbs.R
@@ -15,7 +15,7 @@ prob = weights[,1]*m1 + weights[,2]*m2 + weights[,3]*m3
 
 outcome = rbinom(N,1,prob )
 
-preds = data.frame(m1,m2,m3)
+preds = (data.frame(m1,m2,m3))
 
 dat = makeForecastData(.predCalibration=preds,.outcomeCalibration=outcome)
 this.ensemble <- calibrateEnsemble(dat, model="logit", tol=0.000000000000000001, maxIter=25000, exp=1, useModelParams = TRUE)
@@ -27,8 +27,81 @@ predCalibration = preds
 outcomeCalibration = outcome
 W = c(1/3,1/3,1/3)
 
-x2 = GibbsLogit(outcomeCalibration,as.matrix(predCalibration),W,10000)
+x1 = GibbsLogit(outcomeCalibration,as.matrix(predCalibration),W,10000)
 
+w2 = c(1,0,0)
+x2 = GibbsLogit(outcomeCalibration,as.matrix(predCalibration),w2,10000)
+
+
+
+
+
+.makeAdj <- function(x){
+  .adjPred <- qlogis(x)
+  .negative <- .adjPred<0
+  .pos <- .adjPred>1
+  .adjPred <- ((1+abs(.adjPred))^(1/exp))-1
+  .miss <- is.na(.adjPred)
+  .negative[.miss] <- FALSE
+  .adjPred[.negative] <- .adjPred[.negative]*(-1)
+  #.adjPred[.pos] <- NA
+  .adjPred[.miss] <- NA
+  .adjPred
+}
+.modelFitter <- function(preds){
+  .adjPred <- .makeAdj(preds)
+  .thisModel <- glm(outcomeCalibration~.adjPred, family=binomial(link = "logit"))
+  if (!.thisModel$converged){stop("One or more of the component logistic regressions failed to converge.  This may indicate perfect separtion or some other problem.  Try the useModelParams=FALSE option.")}
+  return(.thisModel)
+}
+
+.predictCal <- function(x){
+  .rawPred <- predict(x, type="response")
+  .outPred <- rep(NA, nObsCal)
+  .outPred[as.numeric(names(.rawPred))] <- .rawPred
+  return(.outPred)
+}
+
+nObsCal = dim(preds)[1]
+nMod = dim(preds)[2]
+nDraws = 1
+exp =3
+.models <- apply(preds, 2, FUN=.modelFitter)
+
+
+predCalibrationAdj <- aperm(array(laply(.models, .predictCal), dim=c(dim(preds)[2], dim(preds)[1], 1)), c(2,1,3))
+  dim(predCalibrationAdj)
+  array(laply(.models, coefficients), dim=c(nMod, 2, nDraws))
+  modelParams <- aperm(array(laply(.models, coefficients), dim=c(nMod, 2, nDraws)), c(2,1,3))
+}
+
+
+
+x2 = GibbsLogit(outcomeCalibration,as.matrix(predCalibrationAdj[,,1]),W,10000)
+
+
+
+data(calibrationSample)
+data(testSample)
+
+preds = calibrationSample[,c("LMER", "SAE", "GLM")]
+outcomeCalibration = calibrationSample[,"Insurgency"]
+
+nObsCal = dim(preds)[1]
+nMod = dim(preds)[2]
+nDraws = 1
+exp =3
+.models <- apply(preds, 2, FUN=.modelFitter)
+
+predCalibrationAdj <- aperm(array(laply(.models, .predictCal), dim=c(dim(preds)[2], dim(preds)[1], 1)), c(2,1,3))
+
+x2 = GibbsLogit(outcomeCalibration,as.matrix(predCalibrationAdj[,,1]),W,20000)
+
+
+
+
+
+preds = predCalibrationAdj[,,1]
 
 iterations = 10000
 w_post = matrix(NA, nrow = iterations, ncol = dim(preds)[2])
@@ -52,9 +125,11 @@ for(iter in 1:iterations){
   w_post[iter,] = W
 }
   
-  
-  
-  x2[["theta_post"]][[1]][696,]
+apply(w_post[6000:10000,],2, mean)  
+apply(x2[[2]][6000:10000,],2, mean)  
+
+
+x2[["theta_post"]][[1]][696,]
 theta[696,]
 x2[["W_post"]]
 
